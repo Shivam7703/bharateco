bf8936163a392e4bd5338dd08b189c8cb48a42b1
shivam7703 <shivamg7703@gmail.com>
2025-01-07 16:46:48 +0530

    foot

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,33 +9,33 @@ import { TbBrandYoutubeFilled } from "react-icons/tb";
 
 export default function Footer({ footer }: any) {
   return (
-    <footer className="pt-5 p-6 md:px-24  w-full z-20  text-white bg-zinc-900  relative">
+    <footer className="pt-5 p-6 md:px-24  w-full z-20  text-white bg-zinc-800  relative">
       
       {/* NewsLetter Section  */}
      
       {/* Footer links section  */}
-      <div className="grid grid-cols-1 pb-5 sm:grid-cols-2 md:grid-cols-4 ">
-        <div className="col-span-1 flex flex-col gap-y-2">
+      <div className="grid grid-cols-2 pb-5 sm:grid-cols-3 md:grid-cols-12">
+        <div className="col-span-3 flex flex-col gap-y-2">
         <Image
           src={footer?.logo}
           alt="logo"
-          className="max-w-64 w-[80vw] object-contain"
+          className="max-w-44 w-[80vw] object-contain"
         />
-        <p className=" w-full  max-sm:mt-3">
+        <p className=" w-full text-base mt-3  max-sm:mt-3">
           {footer?.text}
         </p>
         {/* Socials  */}
         {footer?.socials && (
-          <div className="flex justify-center gap-3 md:gap-x-6 mt-3  text-2xl text-white">
+          <div className="flex  gap-2 md:gap-x-3 mt-3  text-4xl text-white">
             {footer?.socials?.facebook && (
               <FaFacebook
-                className="social-icon hover:text-main duration-300"
+                className=" hover:text-green1 p-2 bg-zinc-700 rounded-full duration-300 "
                 onClick={() => window.open(footer?.socials?.facebook, "_blank")}
               />
             )}
             {footer?.socials?.instagram && (
               <AiFillInstagram
-                className="social-icon hover:text-main duration-300"
+                className="hover:text-green1 p-2 bg-zinc-700 rounded-full duration-300"
                 onClick={() =>
                   window.open(footer?.socials?.instagram, "_blank")
                 }
@@ -43,19 +43,19 @@ export default function Footer({ footer }: any) {
             )}
             {footer?.socials?.linkedin && (
               <FaLinkedinIn
-                className="social-icon hover:text-main duration-300"
+                className="hover:text-green1 p-2 bg-zinc-700 rounded-full duration-300"
                 onClick={() => window.open(footer?.socials?.linkedin, "_blank")}
               />
             )}
             {footer?.socials?.youtube && (
               <TbBrandYoutubeFilled
-                className="social-icon hover:text-main duration-300"
+                className="hover:text-green1 p-2 bg-zinc-700 rounded-full duration-300"
                 onClick={() => window.open(footer?.socials?.youtube, "_blank")}
               />
             )}
             {footer?.socials?.twitter && (
               <FaXTwitter
-                className="social-icon hover:text-main duration-300"
+                className="hover:text-green1 p-2 bg-zinc-700 rounded-full duration-300"
                 onClick={() => window.open(footer?.socials?.twitter, "_blank")}
               />
             )}
@@ -63,30 +63,46 @@ export default function Footer({ footer }: any) {
         )}
         </div>
         <div className="col-span-1 flex flex-col gap-y-2 max-md:hidden"></div>
-        <div className="col-span-1 flex flex-col gap-y-2 mt-10">
+        <div className="col-span-2 flex flex-col gap-y-2 mt-5">
           <h4 className="my-2 text-xl font-semibold">{footer?.list2?.title}</h4>
-          <ul>
+          <ul className="space-y-2">
             {footer?.list2?.links?.map((d: any, i: number) => (
               <li key={i}>
                 <Link
                   href={d?.href}
-                  className=" transition:all text-lg mb-1 text-zinc-200 duration-300 hover: hover:text-main "
+                  className=" transition:all text-base text-zinc-200 duration-300 hover:text-green2 "
                 >
-                  {d?.label}
+                {"-"}  {d?.label}
                 </Link>
               </li>
             ))}
           </ul>
         </div>
-       
+        <div className="col-span-2 flex flex-col gap-y-2 md:pl-14 mt-5">
+          <h4 className="my-2 text-xl font-semibold">{footer?.list2?.title}</h4>
+          <ul className="space-y-2">
+            {footer?.list2?.links?.map((d: any, i: number) => (
+              <li key={i}>
+                <Link
+                  href={d?.href}
+                  className=" transition:all text-base text-zinc-200 duration-300 hover:text-green2 "
+                >
+                {"-"}  {d?.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-span-1 flex flex-col gap-y-2 max-md:hidden"></div>
 
-        <div className=" flex flex-col mt-10 space-y-2  col-span-1">
-          <h4 className="my-2 mb-3 text-xl font-semibold">
+
+        <div className=" flex flex-col mt-5 space-y-2  col-span-3">
+          <h4 className="my-2  text-xl font-semibold">
             {footer?.newLetter?.title}
           </h4>
-          <p className=' transition:all text-lg text-zinc-200 duration-300 hover: hover:text-main '><strong>Phone: </strong>&nbsp;+91 1234567890</p>
-          <p className=' transition:all text-lg text-zinc-200 duration-300 hover: hover:text-main '><strong>E-mail: </strong>&nbsp;info@bharatecofuel.in</p>
-          <p className=' transition:all text-lg text-zinc-200 duration-300 hover: hover:text-main '><strong>Address: </strong>&nbsp;A-60, SECTOR-58 NOIDA, UP-201301, UTTAR PRADESH</p>
+          <p className=' transition:all text-base text-zinc-200 duration-300 hover: hover:text-main '><strong>Phone: </strong>&nbsp;+91 1234567890</p>
+          <p className=' transition:all text-base text-zinc-200 duration-300 hover: hover:text-main '><strong>E-mail: </strong>&nbsp;info@bharatecofuel.in</p>
+          <p className=' transition:all text-base text-zinc-200 duration-300 hover: hover:text-main '><strong>Address: </strong>&nbsp;A-60, SECTOR-58 NOIDA, UP-201301, UTTAR PRADESH</p>
         
         </div>
       </div>
