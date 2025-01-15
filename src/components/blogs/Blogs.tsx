e9aa6f0fc3134fb71e063d1524dcead9c2d415d2
shivam7703 <shivamg7703@gmail.com>
2025-01-15 18:17:16 +0530

    new

--- src/components/blogs/Blogs.tsx
+++ src/components/blogs/Blogs.tsx
@@ -9,7 +9,7 @@ export default function Blogsection({data}:any) {
 
 
   return (
-    <section className="my-6 md:p-24 p-6 text-center relative slider1 flex flex-col items-center">
+    <section className="my-6 md:p-24 py-6 px-2 text-center relative slider1 flex flex-col items-center">
        <div className="w-full text-center flex flex-col items-center !gap-y-4 mb-8 md:mb-16">
                    <h4 className='text-green3 bg-[#E1F1D8] px-7 mb-3 font-bold text-sm rounded-lg py-3 w-max'>Blogs and News </h4>
                      <h2 className='text-zinc-800 text-2xl md:text-4xl font-bold'>Stay Updated With our Latest News And Blogs</h2>
@@ -17,34 +17,34 @@ export default function Blogsection({data}:any) {
                          </div>
       <div
         
-        className=" w-full max-w-fit px-5 mt-6 flex flex-wrap justify-between  gap-y-6"
+        className=" w-full max-w-fit  mt-6 flex flex-wrap justify-between  gap-y-6"
       >
         {" "}
         {data?.blogs?.map((cards: any) => (
           <div
             key={cards.id}
-            className="md:mb-12 w-[49%] md:w-[31%] p-4  rounded-2xl  "
+            className="md:mb-12 w-[50%] md:w-[31%] sm:p-4 p-1  rounded-xl  "
           >
             <div className=" w-full  h-full overflow-hidden  group  shadow-xl bg-white rounded-xl">
-              <div className="relative overflow-hidden w-full h-60">
+              <div className="relative overflow-hidden w-full sm:h-60 h-40">
                 <Image
                   src={cards.img}
                   alt=""
                   className=" transition-all duration-500 w-full h-60 object-cover group-hover:scale-110  z-10"
                 />
               </div>
-              <div className="px-4">
+              <div className="px-1">
               <div className="text-white -mt-7 z-10 sticky bg-green1 py-2 shadow-md px-4 max-w-max text-base ml-4 font-semibold transition-all duration-500 group-hover:bg-zinc-900 ">
                 {cards.date}
               </div>
 
-              <h3 className="text-xl font-bold my-4 text-zinc-800 group-hover:text-green2">
+              <h3 className="md:text-xl text-base font-bold mt-4 text-zinc-800 group-hover:text-green2">
                 {cards.title}
               </h3>
-              <p className="text-zinc-600 text-sm">{cards.text}</p><br />
+              <p className="text-zinc-600 text-sm max-sm:hidden">{cards.text}</p><br />
               <Link 
                   href={"blogs/details"}
-                  className=" duration-500  group-hover:bg-zinc-800 text-white px-16 bg-green3  text-center -mb-3 py-2  text-base  font-bold"
+                  className=" duration-500 max-sm:scale-75  group-hover:bg-zinc-800 text-white px-10 bg-green3  text-center -mb-3 py-2  text-base  font-bold"
                 >
                   Read More
                 </Link><br/><br/>
